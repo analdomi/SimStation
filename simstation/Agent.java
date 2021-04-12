2c6717c3148e43ded20fc2a1c50e89d6b1d21b79
andre <andre.domingo@sjsu.edu>
2021-04-11 19:48:42 -0700

    added state changes to agents

--- simstation/Agent.java
+++ simstation/Agent.java
@@ -7,25 +7,27 @@ public abstract class Agent extends Bean implements Runnable{
     private Heading heading;
     private int xc;
     private int yc;
+    private AgentState state;
 
     public void run() {
+        state = AgentState.RUNNING;
         update();
     }
 
     public void start() {
-
+        state = AgentState.READY;
     }
 
     public void suspend() {
-
+        state = AgentState.SUSPENDED;
     }
 
     public void resume() {
-
+        state = AgentState.READY;
     }
 
    public void stop() {
-
+        state = AgentState.STOPPED;
    }
 
     public void move(int steps) {
