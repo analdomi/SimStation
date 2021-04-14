a411aadc1c0623b98ee7d8a056fb94b51343723a
andre <andre.domingo@sjsu.edu>
2021-04-13 17:14:07 -0700

    finished SimulationFactory

--- simstation/SimulationFactory.java
+++ simstation/SimulationFactory.java
@@ -4,4 +4,39 @@ import mvc.*;
 import simstation.commands.*;
 
 public class SimulationFactory implements AppFactory {
+
+    public Model makeModel() { return new Simulation(); }
+
+    public View makeView(Model m) {
+        return new SimulationView((Simulation)m);
+    }
+
+    public String[] getEditCommands() { return new String[] {"Start", "Suspend", "Resume", "Stop", "Stats"}; }
+
+    public Command makeEditCommand(Model model, String type) {
+        if (type == "Start") {
+            return new StartCommand(model);
+        } else if (type == "Suspend") {
+            return new SuspendCommand(model);
+        } else if (type == "Resume") {
+            return new ResumeCommand(model);
+        } else if (type == "Stop") {
+            return new StopCommand(model);
+        } else if (type == "Stats") {
+            return new StatsCommand(model);
+        }
+        return null;
+    }
+
+    public String getTitle() { return "Simstation"; }
+
+    public String[] getHelp() {
+        return new String[] {""};
+    }
+
+    public String about() {
+        return "Simstation Version 1";
+    }
+
+}
 }
