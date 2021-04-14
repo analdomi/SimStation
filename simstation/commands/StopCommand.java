3c79946d006e5559547581c94d99d00449a8f35d
andre <andre.domingo@sjsu.edu>
2021-04-14 13:30:53 -0700

    added functionality to simstation commands

--- simstation/commands/StopCommand.java
+++ simstation/commands/StopCommand.java
@@ -6,4 +6,11 @@ public class StopCommand extends Command {
     public StopCommand(Model model) {
         super(model);
     }
+
+    public void execute() {
+        Simulation simulation = (Simulation) model;
+
+        simulation.stop();
+        simulation.changed();
+    }
 }
